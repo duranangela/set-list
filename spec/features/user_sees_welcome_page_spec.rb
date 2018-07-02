1ec393d9cd09e0c4465236f3a083c130fcf24b2b
Angela Duran <duranangela19@gmail.com>
2018-07-01 21:40:41 -0600

    Added CSS for songs/index

--- spec/features/user_sees_welcome_page_spec.rb
+++ spec/features/user_sees_welcome_page_spec.rb
@@ -9,12 +9,27 @@ RSpec.describe 'a visitor visiting a welcome page' do
     it 'should show a welcome message' do
 
       visit '/'
-      
-      save_and_open_page
 
       within("#greeting") do
         expect(page).to have_content("Welcome!")
       end
+    end
+    it 'should show a login button' do
+
+      visit '/'
+
+      within('#button') do
+        expect(page).to have_content("Login")
+      end
+    end
+    it 'should go to login page when login is clicked' do
+
+      visit '/'
+      click_button("Login")
+
+      expect(current_path).to eq('/login')
+      expect(page).to have_content('Login information')
+
     end
   end
 end
